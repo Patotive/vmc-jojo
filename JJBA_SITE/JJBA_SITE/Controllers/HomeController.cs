172bd155b9e6b7b2debdf1978ab9030fd98a4df5
Patotive <jaiminhogameplays1009@gmail.com>
2022-12-02 22:17:54 -0300

    upload do projeto
    subindo o projeto para o github pela primeira vez

---
+++ JJBA_SITE/JJBA_SITE/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using JJBA_SITE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JJBA_SITE.Controllers 
+{ 
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Usuario()
+        {
+            var usuario = new Usuario();       // criando objeto para classe
+            return View(usuario);              // retorna para view os dados da classe
+        }
+
+        [HttpPost]
+        public ActionResult Usuario(Usuario usuario)
+        {
+            if (ModelState.IsValid)                            // validando o estado, ou seja, verificando a requisição
+            {
+                return View("Resultado", usuario);             // retorna para a view resultado
+            }
+            return View(usuario);
+        }
+
+        public ActionResult Resultado(Usuario usuario)          //criando a ação para a view usuário
+        {
+            return View(usuario);
+        }
+
+
+    }
+}
---
+++ JJBA_SITE/JJBA_SITE/Models/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JJBA_SITE.Models
+{
+    public class Usuario
+    {
+        [Display(Name = "Digite seu nome:")]
+        [Required(ErrorMessage = "Você não recebeu um nome")]
+        public string NomeUsuario { get; set; }
+
+        [Display(Name = "Personagem ou/e Stand Favoritos:")]
+        [Required(ErrorMessage = "Tem certeza que você não gosta de nenhum personagem? NEM DO SPEEDWAGON?")]
+        public string Favs { get; set; }
+
+        [Display(Name = "Email:")]
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Insira um email válido")]
+        public string Email { get; set; }
+
+        [Display(Name = "Crie seu Login:")]
+        [RegularExpression(@"[a-zA-Z]{5,15}", ErrorMessage = "Somente letras e de 5 a 15 caracteres")]
+        [Required(ErrorMessage = "Login obrigatório")]
+        // [Remote("Login", "Usuario", ErrorMessage = "Usuario já registrado")]
+        public string Login { get; set; }
+
+        [Display(Name = "Crie sua Senha:")]
+        [Required(ErrorMessage = "Senha obrigatório")]
+        public string Senha { get; set; }
+
+        [Display(Name = "Confirmar Senha:")]
+        [System.ComponentModel.DataAnnotations.Compare("Senha", ErrorMessage = "A senha não corresponde")]
+        public string ConfSenha { get; set; }
+        
+    }
+}
